f705a5f2039316a3b94036ec0a0183c60e1d6667
Peter Robinson <peter.robinson@softwire.com>
2020-09-16 16:56:05 +0100

    Refactor station names and codes to mapping

--- SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
@@ -20,7 +20,7 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
     private val job: Job = SupervisorJob()
 
     private val stationCodes = listOf("KGX", "WNS", "WKM", "GLD", "WOK")
-    private val stationNames = mutableListOf<String>()
+    private val stationNameToCodeMapping = mutableMapOf<String, String>()
 
     private var chosenDepartureStation: String = ""
     private var chosenArrivalStation: String = ""
@@ -78,8 +78,8 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
         launch {
             val allStationDetails = getAllStationDetails()
             buildStationNamesList(allStationDetails)
-            view!!.setDepartureDropdown(stationNames)
-            view!!.setArrivalDropdown(stationNames)
+            view!!.setDepartureDropdown(stationNameToCodeMapping.keys.toList())
+            view!!.setArrivalDropdown(stationNameToCodeMapping.keys.toList())
         }
     }
 
@@ -87,7 +87,7 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
         for (stationCode in stationCodes) {
             for (stationInformation in allStationDetails.stations) {
                 if (stationInformation.crs == stationCode) {
-                    stationNames.add(stationInformation.name)
+                    stationNameToCodeMapping[stationInformation.name] = stationCode
                 }
             }
         }
@@ -99,7 +99,13 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
     }
 
     private fun matchStationNameToCode(stationName: String): String {
-        return stationCodes[stationNames.indexOf(stationName)]
+        var matchedCode = ""
+        try {
+            matchedCode = stationNameToCodeMapping[stationName]!!
+        } catch (e: Exception) {
+            view!!.showAlertMessage("Invalid station name")
+        }
+        return matchedCode
     }
 
     private fun buildDepartureInformation(journeyDetails: JourneyDetails): DepartureInformation {
