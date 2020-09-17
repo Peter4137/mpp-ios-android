411949f7358e56c5f45de571dac4114790dda0f4
Jasmine Buckley <jasmine.buckley@softwire.com>
2020-09-17 13:42:04 +0100

    Made advanced search contract and presenter

---
+++ SharedCode/src/commonMain/kotlin/AdvancedSearchPresenter.kt
@@ -0,0 +1,19 @@
+package com.jetbrains.handson.mpp.mobile
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
+
+class AdvancedSearchPresenter: AdvancedSearchContract.Presenter() {
+
+    private val dispatchers = AppDispatchersImpl()
+    private var view: AdvancedSearchContract.View? = null
+    private val job: Job = SupervisorJob()
+
+    override val coroutineContext: CoroutineContext
+        get() = dispatchers.main + job
+
+    override fun onViewTaken(view: AdvancedSearchContract.View) {
+        this.view = view
+    }
+}
